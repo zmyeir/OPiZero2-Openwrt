690ed839ade357b72871a6f41d8e3184d010c893
Kazagumo <13860126292@163.com>
2023-03-05 22:40:48 +0800

    Create init-settings.sh

---
+++ build/Official/files/etc/uci-defaults/99-init-settings/init-settings.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set default theme to luci-theme-argon
+uci set luci.main.mediaurlbase='/luci-static/argon'
+
+# Disable IPV6 ula prefix
+#sed -i 's/^[^#].*option ula/#&/' /etc/config/network
+
+# Check file system during boot
+uci set fstab.@global[0].check_fs=1
+uci commit
+
+exit 0
